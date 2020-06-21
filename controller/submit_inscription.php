a068b3db7e121db345db2b691082be774e027d7b
hg4dacha <hgadacha@student.42.fr>
2020-06-21 21:02:04 +0200

    camagru

--- controller/submit_inscription.php
+++ controller/submit_inscription.php
@@ -4,15 +4,15 @@ $bdd = new PDO ('mysql:host=localhost;dbname=camagru', 'root', 'MAMP93', array(P
 
 if(isset($_POST['submit_inscription']))
 {
+    $lastname = htmlspecialchars($_POST['lastname']);
+    $firstname = htmlspecialchars($_POST['firstname']);
+    $email = htmlspecialchars($_POST['email']);
+    $username = htmlspecialchars($_POST['username']);
+    $password = htmlspecialchars($_POST['password']);
+    $password2 = htmlspecialchars($_POST['password2']);
+
     if(!empty($_POST['lastname']) && !empty($_POST['firstname']) && !empty($_POST['email']) && !empty($_POST['username']) && !empty($_POST['password']) && !empty($_POST['password2']))
     {
-        $lastname = htmlspecialchars($_POST['lastname']);
-        $firstname = htmlspecialchars($_POST['firstname']);
-        $email = htmlspecialchars($_POST['email']);
-        $username = htmlspecialchars($_POST['username']);
-        $password = htmlspecialchars($_POST['password']);
-        $password2 = htmlspecialchars($_POST['password2']);
-
         $lastname_length = strlen($lastname);
         $firstname_length = strlen($firstname);
         $email_length = strlen($email);
@@ -22,35 +22,45 @@ if(isset($_POST['submit_inscription']))
 
         if($lastname_length <= 250 && $firstname_length <= 250 && $email_length <= 250 && $username_length <= 250 && $password_length <= 250 && $password2_length <= 250)
         {
-            if(filter_var($email, FILTER_VALIDATE_EMAIL))
+            if(filter_var($email, FILTER_VALIDATE_EMAIL) && preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $email))
             {
                 if($password == $password2)
                 {
-                    $reqmail = $bdd->prepare('SELECT email FROM users WHERE email = ?');
-                    $retreq = $reqmail->execute(array($email));
-                    print_r($retreq);
-                    if($retreq == 0)
+                    $requsername = $bdd->prepare('SELECT username FROM users WHERE username = ?');
+                    $requsername->execute(array($username));
+                    $usernameExist = $requsername->rowcount();
+                    if($usernameExist == 0)
                     {
+                        $reqmail = $bdd->prepare('SELECT email FROM users WHERE email = ?');
+                        $reqmail->execute(array($email));
+                        $emailExist = $reqmail->rowcount();
+                        if($emailExist == 0)
+                        {
 
+                        }
+                        else
+                        {
+                            $error = "L'adresse e-mail est déjà utilisée. Veuillez insérer une autre adresse e-mail";
+                        }
                     }
                     else
                     {
-                        $error = "L'adresse email est déjà utilisée";
+                        $error = "Le nom d'utilisateur est déjà utilisé. Veuillez insérer un autre nom d'utilisateur";
                     }
                 }
                 else
                 {
-                    $error = "Les mots de passes ne correspondent pas";
+                    $error = "Les mots de passe ne correspondent pas";
                 }
             }
             else
             {
-                $error = "L'adresse e-mail n'est pas valide";
+                $error = "L'adresse e-mail n'est pas valide. (L'adresse e-mail doit uniquement contenir des lettres sans accents, des \".\", des \"-\" et des \"_\")";
             }
         }
         else
         {
-            $error = "Vous ne pouvez entrer que 250 caractères maximum";
+            $error = "Vous ne pouvez entrer que 250 caractères par champ maximum !";
         }
     }
     else
--- view/inscription.php
+++ view/inscription.php
@@ -21,16 +21,16 @@ require('../controller/submit_inscription.php');
                 <form method="post" action="inscription.php">
                     <input class="row" type="text" name="lastname" id="lastname"
                     placeholder="Nom (ex : Dupont)" 
-                    size="30" maxlength="250"/>
-                    <input class="row" type="text" name="firstname" id="name"
+                    size="30" maxlength="250" value="<?php if(isset($lastname)) { echo $lastname; } ?>"/>
+                    <input class="row" type="text" name="firstname" id="firstname"
                     placeholder="Prénom (ex : Jean)" 
-                    size="30" maxlength="250"/>
+                    size="30" maxlength="250" value="<?php if(isset($firstname)) { echo $firstname; } ?>"/>
                     <input class="row" type="email" name="email" id="email"
                     placeholder="Adresse e-mail (jean.dupont@exemple.com)" 
-                    size="30" maxlength="250"/>
+                    size="30" maxlength="250" value="<?php if(isset($email)) { echo $email; } ?>"/>
                     <input class="row" type="text" name="username" id="username"
                     placeholder="Nom d'utilisateur (pseudonyme)" 
-                    size="30" maxlength="250"/>
+                    size="30" maxlength="250" value="<?php if(isset($username)) { echo $username; } ?>"/>
                     <input class="row" type="password" name="password" id="password"
                     placeholder="Mot de passe" size="30" maxlength="250"/>
                     <input id="last" type="password" name="password2" id="password2"
