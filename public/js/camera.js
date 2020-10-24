0129cbfd3b9fe254c7dfb60c4ff78c3b9bad7761
hg4dacha <hgadacha@student.42.fr>
2020-10-24 18:18:02 +0200

    camagru

--- public/js/camera.js
+++ public/js/camera.js
@@ -1,10 +1,17 @@
-//activate the camera
+//   activate/deactivate the camera
 let camStatus = false;
-let buttCam = document.querySelector('#camera');
-buttCam.onclick = statCam();
+const buttCam = document.querySelector('#buttCam');
+buttCam.onclick = () => { statCam(); }
+const video = document.querySelector('video');
+
+const canvas = document.querySelector('#canvas');
+const context = canvas.getContext('2d');
+const width = canvas.width;
+const height = canvas.height;
+
 
 function statCam() {
-    
+
     if (camStatus === false){
         navigator.mediaDevices.getUserMedia({video: true, audio: false})
             .then(function(mediaStream) {
@@ -17,9 +24,18 @@ function statCam() {
                 console.log(`Error : ${err}`);
             });
     }
-    // else if (camStatus === true) {
-    //     const stream = mediaStream;
-    //     const tracks = stream.getTracks();
-    //     tracks[0].stop;
-    // }
+    else if (camStatus === true) {
+        const mediaStream = video.srcObject;
+        const tracks = mediaStream.getTracks();
+        tracks[0].stop();
+        // context.clearRect(0, 0, width, height);
+        camStatus = false;
+        buttCam.innerHTML='Activer la caméra';
+    }
 }
+
+//   canvas
+video.addEventListener('canplay', function flux() {
+    context.drawImage(video, 0, 0, width, height);
+    setTimeout(flux, 1);
+})
