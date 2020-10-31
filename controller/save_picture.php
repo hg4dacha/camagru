a66efe0a79203b86125b83dca9051286c74e7bef
hgadacha <hgadacha@student.42.fr>
2020-10-31 16:52:49 +0100

    camagru

--- controller/save_picture.php
+++ controller/save_picture.php
@@ -2,18 +2,21 @@
 
 session_start();
 
-require_once($_SERVER['DOCUMENT_ROOT']."/camagru/model/dbConnexion.php");
+require_once($_SERVER['DOCUMENT_ROOT']."/camagru/model/sqlFunctions.php");
 
 
     if (isset($_POST['imgData']) && isset($_POST['imgID']))
     {
+        if(!(empty($_POST['imgData']))) $imgData = htmlspecialchars($_POST['imgData']);
+        if(!(empty($_POST['imgID']))) $imgID = htmlspecialchars($_POST['imgID']);
+        
         // $idUsr = $_SESSION['id'];
-
-        $imgData = str_replace('data:image/jpeg;base64,', '', $_POST['imgData']);
+        $imgData = str_replace('data:image/jpeg;base64,', '', $imgData);
         $imgData = base64_decode($imgData);
         $newImg = imagecreatefromstring($imgData);
-        imagejpeg($newImg, '../public/users_pictures/'.$_POST['imgID'].'.jpeg');
+        imagejpeg($newImg, '../public/users_pictures/'.$imgID.'.jpeg');
         imagedestroy($newImg);
+        chmod('../public/users_pictures/'.$imgID.'.jpeg', 0777);
     }
 
 ?>
