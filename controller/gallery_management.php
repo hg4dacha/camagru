270c67cca9c3a49a4dfd9bf3c6a4d962aeb91eb8
hgadacha <hgadacha@student.42.fr>
2020-11-10 21:15:12 +0100

    camagru

--- controller/gallery_management.php
+++ controller/gallery_management.php
@@ -2,9 +2,23 @@
 
 require_once($_SERVER['DOCUMENT_ROOT']."/camagru/model/sqlFunctions.php");
 
-$idUsr = intval($_SESSION['id']);
+if (isset($_GET['page']) && !empty($_GET['page']) && $_GET['page'] > 0) {
+    $_GET['page'] = intval($_GET['page']);
+    $currentPage = $_GET['page'];
+} else {
+    $currentPage = 1;
+}
 
-$pictures = usr_pictr_recup($idUsr);
+$idUsr = intval($_SESSION['id']);
+$picturesInPage = 6;
+$beginning = ($currentPage - 1) * $picturesInPage;
+$picturesNmbr = pictNmbr($idUsr);
+$pictures = usr_pictr_recup($idUsr, $beginning, $picturesInPage);
+
+
+
+
+
 // echo '<pre>';
 // echo($pictures[0]['picture_id']);
 // echo '</pre>';
--- model/sqlFunctions.php
+++ model/sqlFunctions.php
@@ -130,10 +130,21 @@ function pswrd_recuperation($id_usr)
     return $reqCtrl->fetch();
 }
 
-function usr_pictr_recup($id_user) {
+function pictNmbr($id_user)
+{
     $dbc = db_connex();
-    $reqCtrl = $dbc->prepare("SELECT * FROM user_pictures WHERE id_user = :id_user ORDER BY id DESC");
+    $reqField = $dbc->prepare("SELECT id FROM user_pictures WHERE id_user = :id_user");
+    $reqField->bindValue(':id_user', $id_user, PDO::PARAM_INT);
+    $reqField->execute();
+    return $reqField->rowCount();
+}
+
+function usr_pictr_recup($id_user, $beginning, $picturesInPage) {
+    $dbc = db_connex();
+    $reqCtrl = $dbc->prepare("SELECT * FROM user_pictures WHERE id_user = :id_user ORDER BY id DESC LIMIT :beginning, :picturesInPage");
     $reqCtrl->bindValue(':id_user', $id_user, PDO::PARAM_INT);
+    $reqCtrl->bindValue(':beginning', $beginning, PDO::PARAM_INT);
+    $reqCtrl->bindValue(':picturesInPage', $picturesInPage, PDO::PARAM_INT);
     $reqCtrl->execute();
     return $reqCtrl->fetchAll();
 }
