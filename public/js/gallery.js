3f9c812caa26a7e32c5a47204b94612836a6e0c7
hgadacha <hgadacha@student.42.fr>
2020-11-10 12:48:35 +0100

    camagru

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -1,27 +1,19 @@
 let imageUsrDiv = document.querySelectorAll('.picture-div');
 let imageUsr = document.querySelectorAll('.user-pictures');
 let trash = document.querySelectorAll('.delete-img');
-let i;
-let j;
-console.log(imageUsr[0]['id']);
 
+for  (let i = 0; i < imageUsrDiv.length; i++) {
 
-function viewDelete(index) {
-    imageUsr[index].style.filter = 'opacity(40%)';
-    trash[index].style.display = 'initial';
-    console.log('OK');
+    imageUsrDiv[i].addEventListener('mouseover', () => {
+        imageUsr[i].style.filter = 'brightness(30%)';
+        trash[i].style.display = 'initial';
+    });
 }
 
-function maskDelete(index) {
-    imageUsr[index].style.filter = 'opacity(100%)';
-    trash[index].style.display = 'none';
-    console.log('XX');
-}
-
-for ( i = 0; i < imageUsrDiv.length; i++ ) {
-    imageUsrDiv[i].addEventListener( 'mouseover', viewDelete(i) );
-}
-
-for ( j = 0; j < imageUsrDiv.length; j++ ) {
-    imageUsrDiv[j].addEventListener( 'mouseout', maskDelete(j) );
+for (let j = 0; j < imageUsrDiv.length; j++) {
+    
+    imageUsrDiv[j].addEventListener('mouseout', () => {
+        imageUsr[j].style.filter = 'brightness(100%)';
+        trash[j].style.display = 'none';
+    });
 }
